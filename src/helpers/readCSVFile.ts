5bf9b4d7ec8d8361da943ca8b3fbdf22336a74e5
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-27 14:07:17 -0300

    Handles reading data from CSV file

---
+++ src/helpers/readCSVFile.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import csv from 'csv-parser'
+
+import { writeDataToCSV } from './writeDataToCSV'
+import { Ship } from '../types/Ship'
+
+let ships: Ship = []
+
+export async function readCSVFile() {
+  await writeDataToCSV()
+
+  fs.createReadStream('/tmp/ships.csv')
+    .pipe(csv({}))
+    .on('data', (row) => {
+      const ship = {
+        name: row.ship,
+        missions: row.missions.split(';')
+      }
+
+      ships.push(ship)
+    })
+    .on('end', () => {
+      console.table(ships)
+    })
+}
